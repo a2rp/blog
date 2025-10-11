70a8bb131d35e8af3eac65fee4039c20ef2f5927
ash1198 <ash.ranjan09@gmail.com>
2025-10-11 15:41:14 +0530

    Updates

--- assets/index-DjeK6cw5.js
+++ assets/index-DjeK6cw5.js
@@ -1,4 +1,4 @@
-import{d as e,a as r,j as t,N as a}from"./index-xMd1lRd9.js";const n={Wrapper:e.div`
+import{d as e,R as r,j as t,N as a}from"./index-BvFHweZ2.js";const n={Wrapper:e.div`
         background-color: #111;
         padding: 15px;
         display: flex;
